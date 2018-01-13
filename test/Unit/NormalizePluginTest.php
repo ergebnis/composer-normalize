8bbcb657c8326d5023b6e2514b8781cb2963d65c
Andreas Möller <am@localheinz.com>
2018-01-13 18:29:15 +0100

    Enhancement: Implement NormalizePlugin

---
+++ test/Unit/NormalizePluginTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/composer-normalize
+ */
+
+namespace Localheinz\Composer\Normalize\Test\Unit;
+
+use Composer\Composer;
+use Composer\IO;
+use Composer\Plugin;
+use Localheinz\Composer\Normalize\Command\NormalizeCommand;
+use Localheinz\Composer\Normalize\NormalizePlugin;
+use Localheinz\Test\Util\Helper;
+use PHPUnit\Framework;
+
+final class NormalizePluginTest extends Framework\TestCase
+{
+    use Helper;
+
+    /**
+     * @dataProvider providerInterfaceName
+     *
+     * @param string $interfaceName
+     */
+    public function testImplementsPluginInterface(string $interfaceName)
+    {
+        $this->assertClassImplementsInterface($interfaceName, NormalizePlugin::class);
+    }
+
+    public function providerInterfaceName(): \Generator
+    {
+        $interfaces = [
+            Plugin\PluginInterface::class,
+            Plugin\Capable::class,
+            Plugin\Capability\CommandProvider::class,
+        ];
+
+        foreach ($interfaces as $interface) {
+            yield $interface => [
+                $interface,
+            ];
+        }
+    }
+
+    public function testGetCapabilitiesReturnsCapabilities()
+    {
+        $plugin = new NormalizePlugin();
+
+        $plugin->activate(
+            $this->prophesize(Composer::class)->reveal(),
+            $this->prophesize(IO\IOInterface::class)->reveal()
+        );
+
+        $expected = [
+            Plugin\Capability\CommandProvider::class => NormalizePlugin::class,
+        ];
+
+        $this->assertSame($expected, $plugin->getCapabilities());
+    }
+
+    public function testProvidesNormalizeCommand()
+    {
+        $plugin = new NormalizePlugin();
+
+        $plugin->activate(
+            $this->prophesize(Composer::class)->reveal(),
+            $this->prophesize(IO\IOInterface::class)->reveal()
+        );
+
+        $commands = $plugin->getCommands();
+
+        $this->assertCount(1, $commands);
+
+        $command = \array_shift($commands);
+
+        $this->assertInstanceOf(NormalizeCommand::class, $command);
+    }
+}
