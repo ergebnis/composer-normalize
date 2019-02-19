1e050461eb87a64e18b8c55b4e5063b499450d41
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-02-19 08:38:43 +0100

    Issue #171: Add mechanism to resolve the location of the composer.json's schema.

--- src/NormalizePlugin.php
+++ src/NormalizePlugin.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Localheinz\Composer\Normalize;
 
+use Composer\Cache;
 use Composer\Composer;
 use Composer\Factory;
 use Composer\IO;
@@ -31,12 +32,26 @@ final class NormalizePlugin implements Plugin\PluginInterface, Plugin\Capable, P
      */
     private $io;
 
+    /**
+     * @var string
+     */
+    private $composerJsonSchema;
+
+    /**
+     * {@inheritdoc}
+     */
     public function activate(Composer $composer, IO\IOInterface $io): void
     {
         $this->composer = $composer;
         $this->io = $io;
+
+        // Statically cache the composerJsonSchema if any.
+        $this->composerJsonSchema = $this->getComposerJsonSchema();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getCapabilities(): array
     {
         return [
@@ -44,13 +59,85 @@ final class NormalizePlugin implements Plugin\PluginInterface, Plugin\Capable, P
         ];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getCommands(): array
     {
+        $file = null;
+        if ($local = $this->getComposerJsonSchema()) {
+            $file = 'file://' . $local;
+        }
+
         return [
             new Command\NormalizeCommand(
                 new Factory(),
-                new Normalizer\ComposerJsonNormalizer()
+                new Normalizer\ComposerJsonNormalizer($file)
             ),
         ];
+    }
+
+    /**
+     * TODO
+     */
+    private function getComposerJsonSchema() {
+        static $composerJsonSchema;
+
+        if (isset($composerJsonSchema)) {
+            return $composerJsonSchema;
+        }
+
+        if ($file = $this->getAndCacheRemoteSchemaFile()) {
+            $composerJsonSchema = $file;
+
+            return $composerJsonSchema;
+        }
+
+        if ($file = $this->getLocalSchemaFromVendor()) {
+            $composerJsonSchema = $file;
+
+            return $composerJsonSchema;
+        }
+
+        // Todo
+    }
+
+    /**
+     * TODO
+     */
+    private function getLocalSchemaFromVendor() {
+        $file = __DIR__ . '/../../composer/res/composer-schema.json';
+
+        return file_exists($file) ?
+            realpath($file):
+            false;
+    }
+
+    /**
+     * TODO
+     */
+    private function getAndCacheRemoteSchemaFile() {
+        $config = $this->composer->getConfig();
+
+        $cache = new Cache($this->io, $config->get('cache-dir'));
+
+        // Official url of the composer.json schema.
+        $url = 'https://getcomposer.org/schema.json';
+
+        $read = $cache->read('composer-schema.json');
+        $tmpFilename = $config->get('cache-dir') . '/composer-schema.json';
+
+        if (false === $read) {
+            $rfs = Factory::createRemoteFilesystem($this->io, $config);
+            $rfs->copy($url, $url, $tmpFilename, false, []);
+            $cache->copyFrom('composer-schema.json', $tmpFilename);
+            $read = $cache->read('composer-schema.json');
+        }
+
+        if (false !== $read) {
+            return realpath($tmpFilename);
+        }
+
+        return false;
     }
 }
