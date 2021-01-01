e452c54303b6078bbf668bfdba2e3ded2efb1b54
Andreas Möller <am@localheinz.com>
2021-01-01 13:57:26 +0100

    Enhancement: Update license year

--- test/Integration/Command/NormalizeCommand/Extra/NotValid/IndentSize/NotInteger/Test.php
+++ test/Integration/Command/NormalizeCommand/Extra/NotValid/IndentSize/NotInteger/Test.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2018-2020 Andreas Möller
+ * Copyright (c) 2018-2021 Andreas Möller
  *
  * For the full copyright and license information, please view
  * the LICENSE.md file that was distributed with this source code.
