a2b389103422ed31e92446e22d5402e15325dfed
Andreas Möller <am@localheinz.com>
2023-09-18 16:43:48 +0200

    Fix: Adjust configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -26,9 +26,13 @@ $license = License\Type\MIT::markdown(
 
 $license->save();
 
-$config = PhpCsFixer\Config\Factory::fromRuleSet(new PhpCsFixer\Config\RuleSet\Php81($license->header()), [
-    'no_useless_concat_operator' => false,
-]);
+$ruleSet = PhpCsFixer\Config\RuleSet\Php81::create()
+    ->withHeader($license->header())
+    ->withRules(PhpCsFixer\Config\Rules::fromArray([
+        'no_useless_concat_operator' => false,
+    ]));
+
+$config = PhpCsFixer\Config\Factory::fromRuleSet($ruleSet);
 
 $config->getFinder()
     ->exclude([
